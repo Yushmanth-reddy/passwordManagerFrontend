0d06d2bd2ec1a768f8a8c5699b6f61c803599600
yushmanth <yushmanth@yushmanth-pc.com>
2023-02-11 19:57:50 +0530

    router project ends

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import About from "./pages/about";
+import Home from "./pages/home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Router app</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">about</NavLink>
+        </nav>
       </header>
-    </div>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
